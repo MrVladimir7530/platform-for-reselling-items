0726c569a1ac87e28a3825b8dc7924ee6a72230d
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-12-16 16:29:34 +0300

    Added 5 Dto

---
+++ src/main/java/ru/skypro/homework/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class CommentDto {
+    private int author;
+    private String authorImage;
+    private String authorFirstName;
+    private LocalDate createdAt;
+    private int pk;
+    private String text;
+}
