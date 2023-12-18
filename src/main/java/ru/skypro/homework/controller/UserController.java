9f1cf9ebee1bedb8f77e9d1607ad80ac18bcb916
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-12-18 10:26:22 +0300

    Added methods in UserController

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,10 +1,15 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDto;
+import ru.skypro.homework.dto.UpdateUserDto;
+import ru.skypro.homework.dto.UserDto;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -13,11 +18,28 @@ import ru.skypro.homework.dto.NewPasswordDto;
 @RequestMapping("/users")
 public class UserController {
 
+
     @PostMapping("/set_password")
     public ResponseEntity<Void> setPassword(@RequestBody NewPasswordDto newPasswordDto) {
+        //todo дописать метод setPassword
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getInfoUser() {
+        //todo дописать метод getInfoUser
+        return null;
+    }
 
+    @PatchMapping("/me")
+    public ResponseEntity<UpdateUserDto> updateInfoUser(@RequestBody UpdateUserDto updateUserDto) {
+        //todo дописать метод updateInfoUser
+        return null;
+    }
 
+    @PatchMapping("/me/image")
+    public ResponseEntity<Void> updateAvatarUser(@RequestParam MultipartFile image) {
+        //todo дописать метод updateAvatarUser
+        return null;
+    }
 }
