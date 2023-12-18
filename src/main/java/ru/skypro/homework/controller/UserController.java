8503becca1934bac0d6da351ca62f9c36de8d2ca
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-12-18 08:36:38 +0300

    Added one method in controller UserController

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,23 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.NewPasswordDto;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/set_password")
+    public ResponseEntity<Void> setPassword(@RequestBody NewPasswordDto newPasswordDto) {
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
