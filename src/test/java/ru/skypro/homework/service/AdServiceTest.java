265d2ad644aec5c5fbb8baef60315c616e8ec6ae
MrVladimir <vladimir.volkov-2013@yandex.ru>
2024-01-09 17:47:50 +0300

    fix test add getMyAds

--- src/test/java/ru/skypro/homework/service/AdServiceTest.java
+++ src/test/java/ru/skypro/homework/service/AdServiceTest.java
@@ -3,6 +3,10 @@ package ru.skypro.homework.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import ru.skypro.homework.dto.AdDto;
+import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.mapper.AdMapper;
 import ru.skypro.homework.model.Role;
 import ru.skypro.homework.entity.AdEntity;
 import ru.skypro.homework.entity.ImageEntity;
@@ -12,6 +16,9 @@ import ru.skypro.homework.repository.ImagesRepository;
 import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.impl.AdServiceImpl;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,17 +29,21 @@ public class AdServiceTest {
     private AdRepository adRepositoryMock;
     private UserRepository userRepositoryMock;
     private ImagesRepository imagesRepositoryMock;
-    private AdService out;
+    private AdService adService;
     private final AdEntity adEntityInit = new AdEntity();
     private final UserEntity user = new UserEntity();
     private final ImageEntity imageInit = new ImageEntity();
+    Principal principal;
+    private String username = "UserTest";
+    AdMapper instance = Mappers.getMapper(AdMapper.class);
+
 
     @BeforeEach
     public void init() {
         adRepositoryMock = mock(AdRepository.class);
         userRepositoryMock = mock(UserRepository.class);
         imagesRepositoryMock = mock(ImagesRepository.class);
-        out = new AdServiceImpl(userRepositoryMock, adRepositoryMock, imagesRepositoryMock);
+        adService = new AdServiceImpl(userRepositoryMock, adRepositoryMock, imagesRepositoryMock);
 
         imageInit.setId(1);
         imageInit.setPath("Какой-то путь");
@@ -40,7 +51,7 @@ public class AdServiceTest {
         imageInit.setContentType("Какой-то ContentType");
 
         user.setId(1);
-        user.setUsername("UserTest");
+        user.setUsername(username);
         user.setFirstName("User");
         user.setLastName("Test");
         user.setPassword("password");
@@ -55,17 +66,34 @@ public class AdServiceTest {
         adEntityInit.setTitle("title");
         adEntityInit.setDescription("Description");
         adEntityInit.setUsersByAuthorId(user);
+        principal = () -> username;
     }
 
     @Test
     public void shouldCorrectResultFromMethodFindById() {
         when(adRepositoryMock.findById(anyInt())).thenReturn(Optional.of(adEntityInit));
 
-        assertEquals(adEntityInit, out.findById(1));
+        assertEquals(adEntityInit, adService.findById(1));
         verify(adRepositoryMock, times(1)).findById(1);
 
+    }
 
+    @Test
+    public void getMyAdsTest() {
+        List<AdEntity> result = new ArrayList<>();
+        result.add(adEntityInit);
+        when(userRepositoryMock.findByUsername(anyString())).thenReturn(user);
+        when(adRepositoryMock.findByUsersByAuthorId(any(UserEntity.class))).thenReturn(result);
 
+        AdsDto myAds = adService.getMyAds(principal);
+
+        AdsDto adsDto = new AdsDto();
+        List<AdDto> adDtoList = new ArrayList<>();
+        AdDto adDto = instance.adEntityToAdDto(adEntityInit);
+        adDtoList.add(adDto);
+        adsDto.setResults(adDtoList);
+        adsDto.setCount(adDtoList.size());
+        assertEquals(adsDto, myAds);
     }
 
 }
